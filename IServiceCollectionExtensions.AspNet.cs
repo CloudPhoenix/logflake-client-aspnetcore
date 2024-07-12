42b86234eeb4870af5dad9c96a1bc3cece8eeca1
Matteo Tammaccaro <m.tammaccaro@cloudphoenix.it>
2024-07-12 08:57:44 +0200

    :sparkles: Add customizable Correlation Service

--- IServiceCollectionExtensions.AspNet.cs
+++ IServiceCollectionExtensions.AspNet.cs
@@ -1,16 +1,37 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using NLogFlake.Models.Options;
+using NLogFlake.Services;
 
 namespace NLogFlake;
 
 public static class IServiceCollectionExtensionsAspNet
 {
-    public static IServiceCollection ConfigureLogFlakeMiddlewareOptions(this IServiceCollection services, IConfiguration configuration)
+    public static IServiceCollection ConfigureLogFlakeMiddlewareOptions(this IServiceCollection services, IConfiguration configuration, CorrelationType correlationType = CorrelationType.Guid)
     {
         services.AddScoped<IParameterService, ParameterService>();
+        
+        TryRegisterCorrelationService(services, correlationType);
+
         _ = services.Configure<LogFlakeMiddlewareSettingsOptions>(configuration.GetSection(LogFlakeMiddlewareSettingsOptions.SectionName));
 
         return services;
+    }
+
+    private static void TryRegisterCorrelationService(IServiceCollection services, CorrelationType correlationType)
+    {
+        if (correlationType == CorrelationType.Guid)
+        {
+            services.AddScoped<ICorrelationService, GuidCorrelationService>();
+        }
+        else if (correlationType == CorrelationType.TraceIdentifier)
+        {
+            services.AddHttpContextAccessor();
+            services.AddScoped<ICorrelationService, TraceIdentifierCorrelationService>();
+        }
+        else
+        {
+            throw new ArgumentException("CorrelationType not supported.", nameof(correlationType));
+        }
     }
 }
