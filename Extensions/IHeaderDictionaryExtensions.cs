16e11bf7fb7b5e725f4101e66a042a79cc30aa2d
Matteo Tammaccaro <m.tammaccaro@cloudphoenix.it>
2025-03-10 12:24:35 +0100

    ✨ Create extension method for header masking

---
+++ Extensions/IHeaderDictionaryExtensions.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.Net.Http.Headers;
+
+namespace NLogFlake.Extensions;
+
+internal static class IHeaderDictionaryExtensions
+{
+    internal static IHeaderDictionary MaskAuthorizationHeader(this IHeaderDictionary headers)
+    {
+        if (!headers.Any(h => h.Key == HeaderNames.Authorization)) return headers;
+
+        string auth = headers[HeaderNames.Authorization]!;
+        if (auth.Length > 14)
+        {
+            headers[HeaderNames.Authorization] = auth[..10] + "…" + auth[^3..];
+        }
+
+        return headers;
+    }
+}
