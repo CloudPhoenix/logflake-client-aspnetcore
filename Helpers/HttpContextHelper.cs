37648a1b95b635efcfd3f3adba2787c8404b951d
Matteo Tammaccaro <m.tammaccaro@cloudphoenix.it>
2025-11-20 17:29:41 +0100

    ✨ Remove file contents when logging body

--- Helpers/HttpContextHelper.cs
+++ Helpers/HttpContextHelper.cs
@@ -1,5 +1,7 @@
+using System.Text;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.Net.Http.Headers;
 using NLogFlake.Extensions;
 
 using static NLogFlake.Constants.LogParameterConstants;
@@ -17,7 +19,7 @@ internal static class HttpContextHelper
             { RequestHeadersKey, httpContext.Request.Headers.MaskAuthorizationHeader() },
         };
 
-        string? request = await GetStringBodyAsync(httpContext.Request.Body);
+        string? request = await GetStringBodyAsync(httpContext.Request);
         if (!string.IsNullOrWhiteSpace(request))
         {
             logParameters.Add(RequestBodyKey, request);
@@ -37,7 +39,19 @@ internal static class HttpContextHelper
         return logParameters;
     }
 
-    internal static async Task<string> GetStringBodyAsync(Stream body)
+    private static async Task<string> GetStringBodyAsync(HttpRequest request)
+    {
+        string bodyContent = await GetStringBodyAsync(request.Body);
+
+        if (bodyContent.Contains(HeaderNames.ContentDisposition))
+        {
+            return RemoveFileContent(request.Headers[HeaderNames.ContentType]!, bodyContent);
+        }
+
+        return bodyContent;
+    }
+
+    private static async Task<string> GetStringBodyAsync(Stream body)
     {
         using StreamReader bodyStream = new(body);
         bodyStream.BaseStream.Seek(0, SeekOrigin.Begin);
@@ -47,5 +61,32 @@ internal static class HttpContextHelper
         body.Position = 0;
 
         return stringContent;
+    }
+
+    private static string RemoveFileContent(string contentTypeHeader, string bodyContent)
+    {
+        string boundary = contentTypeHeader.Substring(contentTypeHeader.IndexOf("boundary=") + 9);
+
+        int contentDispositionIndex = bodyContent.IndexOf(HeaderNames.ContentDisposition);
+        string contentDisposition = bodyContent.Substring(contentDispositionIndex, bodyContent.IndexOf("\r", contentDispositionIndex) - contentDispositionIndex);
+
+        int contentTypeIndex = bodyContent.IndexOf(HeaderNames.ContentType);
+        string contentType = bodyContent.Substring(contentTypeIndex, bodyContent.IndexOf("\r", contentTypeIndex) - contentTypeIndex);
+
+        int closingBoundaryIndex = bodyContent.IndexOf(boundary, contentTypeIndex);
+        int fileByteStart = contentTypeIndex + contentType.Length;
+        int fileByteLength = closingBoundaryIndex - (contentTypeIndex + contentType.Length + 2);
+
+        string fileContent = bodyContent.Substring(fileByteStart, fileByteLength).Trim();
+
+        StringBuilder emptyFileUploadBody = new();
+        emptyFileUploadBody.AppendLine(boundary);
+        emptyFileUploadBody.AppendLine(contentDisposition);
+        emptyFileUploadBody.AppendLine(contentType);
+        emptyFileUploadBody.AppendLine($"<{fileContent.Length} bytes omitted>");
+        emptyFileUploadBody.AppendLine();
+        emptyFileUploadBody.AppendLine($"{boundary}--");
+
+        return emptyFileUploadBody.ToString();
     }
 }
